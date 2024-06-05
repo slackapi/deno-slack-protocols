cfb79cb6a6ef5ec0f878b0c92879307db8ef5225
Filip Maj <fmaj@slack-corp.com>
2024-06-05 11:33:46 -0400

    Use dev deps appropriately.

--- src/dev_deps.ts
+++ src/dev_deps.ts
@@ -5,6 +5,7 @@ export {
 export type { Spy } from "https://deno.land/std@0.177.0/testing/mock.ts";
 export {
   assertEquals,
+  assertMatch,
   assertNotEquals,
   assertThrows,
 } from "https://deno.land/std@0.177.0/testing/asserts.ts";
--- src/tests.ts
+++ src/tests.ts
@@ -1,6 +1,6 @@
-import { assertMatch } from "https://deno.land/std@0.177.0/testing/asserts.ts";
 import {
   assertEquals,
+  assertMatch,
   assertNotEquals,
   assertSpyCall,
   assertThrows,
