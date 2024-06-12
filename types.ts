29ba701b1bfe03ccce30b87ebcc819df973a8d5d
Filip Maj <fmaj@slack-corp.com>
2024-06-12 09:39:25 -0400

    Add deno.land shim modules to root of project

---
+++ types.ts
@@ -0,0 +1,4 @@
+// This file only exists as a 'shim' for deno.land, as this package was
+// previously published to deno.land to only include the contents of the `./src`
+// subdirectory.
+export * from './src/types.ts';
